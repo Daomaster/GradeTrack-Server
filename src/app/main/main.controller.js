f0a839e41ee23a1592e273236d9d3e17c9d2788e
Daoyun Zeng <Daoyun Zeng>
2016-04-03 23:02:04 -0700

    Add Gulp Angular Jasmin Karma and all other dependencies to the project.

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('GradeTrack')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+
+  }
+})();
