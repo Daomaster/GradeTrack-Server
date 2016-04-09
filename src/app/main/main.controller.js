52b9e1ab5ee2811b759cf98c902f54643fdf1628
Brandon Stenen <Brandon Stenen>
2016-04-09 02:36:19 -0700

    nav and sidebar added with routing and mock data. need to add assignment sidebar details.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,6 +7,89 @@
 
   /** @ngInject */
   function MainController() {
+    var vm = this;
+
+    vm.activeTab = 0;
+
+
+    vm.lastName = "Name";
+    vm.firstName = "Prof";
+    vm.courses = [
+      {
+        id: 0,
+        name : "CS470",
+        classText: "active",
+        assignments: [
+          {
+            name: "Assignment 1"
+          },
+          {
+            name: "Assignment 2"
+          },
+          {
+            name: "Assignment 3"
+          },
+          {
+            name: "Assignment 4"
+          },
+          {
+            name: "Assignment 5"
+          }
+        ]
+      },
+      {
+        id: 1,
+        name : "CS471",
+        classText: "",
+        assignments: [
+          {
+            name: "Assignment 1b"
+          },
+          {
+            name: "Assignment 2b"
+          },
+          {
+            name: "Assignment 3b"
+          },
+          {
+            name: "Assignment 4b"
+          },
+          {
+            name: "Assignment 5b"
+          }
+        ]
+      },
+      {
+        id: 2,
+        name : "CS472",
+        classText: "",
+        assignments: [
+          {
+            name: "Assignment 1c"
+          },
+          {
+            name: "Assignment 2c"
+          },
+          {
+            name: "Assignment 3c"
+          },
+          {
+            name: "Assignment 4c"
+          },
+          {
+            name: "Assignment 5c"
+          }
+        ]
+      }
+    ];
+    vm.activeCourse = vm.courses[0];
+
+    vm.changeActiveCourse = function(course)
+    {
+      vm.activeCourse.classText = "";
+      vm.activeCourse = course;
+      vm.activeCourse.classText = "active";
+    }
 
   }
 })();
---
+++ src/app/students/students.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('GradeTrack')
+    .controller('StudentsController', StudentsController);
+
+  /** @ngInject */
+  function StudentsController() {
+
+  }
+})();
