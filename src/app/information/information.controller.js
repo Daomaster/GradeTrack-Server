9b441a378ed8f865531f71120d328552a2602f9b
Daoyun Zeng <wenit123@gmail.com>
2016-04-14 19:00:32 -0700

    basic info

--- src/app/information/information.controller.js
+++ src/app/information/information.controller.js
@@ -1,12 +1,26 @@
-(function() {
-  'use strict';
+angular
+  .module('GradeTrack')
+  .controller('InformationController', ["$scope",function($scope){
 
-  angular
-    .module('GradeTrack')
-    .controller('InformationController', InformationController);
+    $scope.classInfo = {
+      id: 'CS 456',
+      name: 'Automata and Formal Languages',
+      info: 'Regular expressions. Regular, context-free, and unrestricted grammars. Finite and pushdown autoamata. Turing machines and the halting problem; introduction to decidability.',
+      pre: 'CS 302 and MATH 351'
+    };
 
-  /** @ngInject */
-  function InformationController() {
+    $scope.profile = {
+      firstName:'Iting',
+      lastName: 'Wen',
+      id: '1244444'
+    }
 
-  }
-})();
+    $scope.syllabus = [{
+      classSection: '1001',
+      classHour:'MW 2:30 ~ 3:45'
+    },{
+      classSection: '1002',
+      classHour:'TT 10:00 ~ 11:15'
+    }]
+
+  }])
