0376a85302d537393cdfe7e96408c438f7e97842
Brandon Stenen <Brandon Stenen>
2016-04-17 04:04:59 -0700

    added angular-chart.js dependancy, and graph display inside assignments.

--- src/app/assignments/assignments.controller.js
+++ src/app/assignments/assignments.controller.js
@@ -8,8 +8,7 @@
   /** @ngInject */
   function AssignmentsController(GradeService) {
     var vm = this;
-
-    vm.showDetails = false;
+    vm.showDetails = true;
     vm.activeCourse = function() { return GradeService.getActiveCourse()};
     vm.addAssignment = function(assignmentName) { GradeService.addAssignment(vm.activeCourse(), "Enter Description", assignmentName)};
 
@@ -23,7 +22,35 @@
         vm.showDetails = true;
     };
 
-    vm.courses = GradeService.courses;
+    vm.getGradeIndex = function(percent)
+    {
+      if (percent >= 0.9)
+        return 0;
+      else if (percent >= 0.8)
+        return 1;
+      else if (percent >= 0.7)
+        return 2;
+      else if (percent >= 0.6)
+        return 3;
+      return 4;
+    };
+    vm.changeGradeArray = function(assignment, student)
+    {
+      var id = assignment.id;
+      var newGrade = student.assignmentGrades[id];
+      var oldGrade = student.oldAssignmentGrades[id];
+      var oldIndex = 4;
+      if (oldGrade != null)
+        oldIndex = vm.getGradeIndex(oldGrade / assignment.points);
+      var newIndex = 4;
+      if (newGrade != null)
+        newIndex = vm.getGradeIndex(newGrade / assignment.points);
+        --assignment.gradeArray[oldIndex];
+        ++assignment.gradeArray[newIndex];
+
+    };
+
+    vm.labelArray = [ "A","B","C","D","F" ];
 
   }
 })();
--- src/app/services.js
+++ src/app/services.js
@@ -58,13 +58,15 @@
           studentID: id_,
           id: course.students.length,
           assignmentGrades: [],
+          oldAssignmentGrades: [],  //graph purposes
           overallGrade: 0
         };
-        for (var i = 0; i < course.assignments.length; ++i)
+        for (var i = 0; i < course.assignments.length; ++i) {
           t.assignmentGrades.push(0);
+          t.oldAssignmentGrades.push(0);
+        }
         course.students.push(t);
       };
-
       this.addAssignment = function(course, description_, assignmentName)
       {
         var t = {
@@ -74,7 +76,8 @@
           id: course.assignments.length,
           points: 500,
           type: "Test",
-          dueDate: new Date()
+          dueDate: new Date(),
+          gradeArray: [0,0,0,0,0] //for graph display
         };
         course.assignments.push(t);
       };
@@ -92,6 +95,10 @@
         for (var k = 0; k < 30; ++k)
         {
           this.addStudent(c, "Student " + k.toString(), 1000000+ j);
+        }
+        for (var l = 0; l < 5; ++l)
+        {
+          c.assignments[l].gradeArray[4] = c.students.length; //initial all to F
         }
       }
 
