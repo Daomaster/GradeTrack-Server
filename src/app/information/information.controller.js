0c1a5188e184305dcbaede737aa4765425a37801
Daoyun Zeng <wenit123@gmail.com>
2016-04-17 20:29:13 -0700

    add disable button to edit info

--- src/app/information/information.controller.js
+++ src/app/information/information.controller.js
@@ -6,21 +6,31 @@ angular
       id: 'CS 456',
       name: 'Automata and Formal Languages',
       info: 'Regular expressions. Regular, context-free, and unrestricted grammars. Finite and pushdown autoamata. Turing machines and the halting problem; introduction to decidability.',
-      pre: 'CS 302 and MATH 351'
+      // pre: 'CS 302 and MATH 351'
     };
 
     $scope.profile = {
-      firstName:'Iting',
-      lastName: 'Wen',
-      id: '1244444'
+      firstName:' Lawrence ',
+      lastName:' Larmore ',
+      // id: ' 1244444 ',
+      phone: '702-895-1096',
+      email:' larmore@egr.unlv.edu',
+      office: ' TBEB378B '
     }
 
-    $scope.syllabus = [{
-      classSection: '1001',
-      classHour:'MW 2:30 ~ 3:45'
-    },{
-      classSection: '1002',
-      classHour:'TT 10:00 ~ 11:15'
-    }]
+    $scope.editInfo = false;
+
+    $scope.toggleDisable = function()
+    {
+      $scope.editInfo = !$scope.editInfo;
+    }
+
+    // $scope.syllabus = [{
+    //   classSection: '1001',
+    //   classHour:'MW 2:30 ~ 3:45'
+    // },{
+    //   classSection: '1002',
+    //   classHour:'TT 10:00 ~ 11:15'
+    // }]
 
   }])
