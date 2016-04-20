a4e54e8027a36ab00ef6a7fdbfad9797256ba6a3
wenit123 <wenit123@gmail.com>
2016-04-20 01:43:16 -0700

    edit css, fix margin and button

--- src/app/information/information.controller.js
+++ src/app/information/information.controller.js
@@ -15,7 +15,7 @@ angular
       phone: '702-895-1096',
       email:' larmore@egr.unlv.edu',
       office: ' TBEB 378B ',
-      hours: 'Mon & Wed  2:00~2:30 '
+      hours: 'Mon & Wed:  2:00~2:30 '
     }
 
     $scope.editInfo = false;
