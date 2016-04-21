d066ef13d6266033303223333c73bd77ed2e9c77
Daoyun Zeng <daoyun.zeng@gmail.com>
2016-04-20 18:07:24 -0700

    Add all the routes to the server

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var auth = require('./server/auth');
+var info = require('./server/info');
+var grade = require('./server/grade');
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -16,6 +21,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(express.static(path.join(__dirname, 'bower_components')));
+
+app.use('/api/info',info);
+app.use('/api/grade',grade);
+app.use('/api/auth',auth);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET the data from ID. */
-router.get('/api/:id', function(req, res, next) {
-  
-});
-
-module.exports = router;
