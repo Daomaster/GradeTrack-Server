00f51c00e3021bc22d9539c8e2789419a8a345aa
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-05-02 11:16:50 -0700

    Put in table

--- src/app/information/information.controller.js
+++ src/app/information/information.controller.js
@@ -13,6 +13,8 @@
 
     vm.instName = function() { return GradeService.lastName + " " + GradeService.firstName };
 
+    vm.email = function() { return GradeService.lastName + GradeService.firstName[0] + "@unlv.edu" }
+
     // TODO: Connections to service
 
   }
