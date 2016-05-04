e2e7965559d842667c6b67637b5ae57c0d90c9c6
Daoyun Zeng <daoyun.zeng@gmail.com>
2016-05-04 13:53:10 -0700

    Allow same origin

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ app.use('/api/info',info);
 app.use('/api/grade',grade);
 app.use('/api/auth',auth);
 
+app.use(function(req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'https://grade-server.herokuapp.com');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        // Pass to next layer of middleware
+        next();
+    });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
